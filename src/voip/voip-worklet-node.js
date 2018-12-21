ba8c39c4cf72bc5874cdd04d384ef721af46c001
James Baicoianu <james_github@baicoianu.com>
2018-12-20 19:15:57 -0800

    Removed typeof check which breaks chrome

--- src/voip/voip-worklet-node.js
+++ src/voip/voip-worklet-node.js
@@ -1,7 +1,3 @@
-if (typeof AudioWorkletNode == 'undefined') {
-  console.warn('Tried to init AudioWorkletNode, but this browser doesn\'t support it');
-  var AudioWorkletNode = class { }
-}
 export class VOIPWorkletNode extends AudioWorkletNode {
   constructor(context, processor) {
     super(context, processor);
